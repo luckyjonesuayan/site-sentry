7347bb44d28fdb4cde69e7175a8671a9a2e0b782
Lucky Jones <luckyjonesbuayan@gmail.com>
2023-06-20 13:53:53 +0800

    updated license and refactored FileReader
    - added copyright
    - added meaningful comments in FileReader.py

--- custom_modules/models/FileReader.py
+++ custom_modules/models/FileReader.py
@@ -8,6 +8,18 @@ class FileReader:
         self._websites = []
         return
     
+    # public
+    def get_website_list(self):
+        # get the websites from external file and convert it to Website object
+        websites_str = self._website_list()
+        for website in websites_str:
+            if self._is_valid_url(website) or self._is_valid_domain_name(website):
+                # only append the website if its a valid url or domain
+                self._websites.append(Website(website))
+        return self._websites
+    
+    # private
+    # helper functions
     def _website_list(self):
         # returns the website list contents
         try:
@@ -23,15 +35,23 @@ class FileReader:
             contents = file.read()
             return contents.splitlines()
         
-    def get_website_list(self):
-        # get the websites from external file and convert it to Website object
-        websites_str = self._website_list()
-        for website in websites_str:
-            if self._is_valid_url(website) or self._is_valid_domain_name(website):
-                # only append the website if its a valid url or domain
-                self._websites.append(Website(website))
-        return self._websites
+    # validators
+    def _is_valid_url(self, url):
+        # check if valid url
+        parsed_url = urlparse(url)
+        return all([parsed_url.scheme, parsed_url.netloc])
     
+    def _is_valid_domain_name(self, domain):
+        # check if the content is a valid domain
+        extracted = tldextract.extract(domain)
+        return all([extracted.domain, extracted.suffix])
+    
+    def _get_time_duration_str(self):
+        with open("website_list.txt", "r") as file:
+            time_str = file.read()
+            return
+        
+    # generators
     def _generate_initial_website_list(self):
         # ensure that to remove the leading whitespace of the initial message
         initial_message = textwrap.dedent("""
@@ -54,23 +74,5 @@ class FileReader:
             file.write("# WEBSITE LIST - List the websites you want to be blocked below this line:\n\n")
             file.write(initial_message)
     
-    
-    def _is_valid_url(self, url):
-        # check if valid url
-        parsed_url = urlparse(url)
-        return all([parsed_url.scheme, parsed_url.netloc])
-    
-    def _is_valid_domain_name(self, domain):
-        # check if the content is a valid domain
-        extracted = tldextract.extract(domain)
-        return all([extracted.domain, extracted.suffix])
-    
-    def _get_time_duration_str(self):
-        with open("website_list.txt", "r") as file:
-            time_str = file.read()
-            return
-        
-    
-    
-    
-        
+    def _generate_initial_config(self):
+        return    
